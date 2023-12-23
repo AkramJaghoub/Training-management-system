caac726fb5d333b4ae0f2e83093fce5a9c1db4c0
AkramJaghoub <akram.jaghoub55@gmail.com>
2023-12-23 14:43:20 +0300

    feat: admin dashboard v2 with data fetched from backend

--- src/main/java/ju/example/training_management_system/controller/AdminController.java
+++ src/main/java/ju/example/training_management_system/controller/AdminController.java
@@ -1,7 +1,10 @@
 package ju.example.training_management_system.controller;
 
+import ju.example.training_management_system.service.AdminService.AdminService;
+import ju.example.training_management_system.service.company.CompanyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final AdminService adminService;
+
     @GetMapping("/dashboard")
-    public String setUpAdminDashboard(){
+    public String setUpAdminDashboard(Model model){
+        adminService.setUpAdminDashboard(model);
         return "/admin/admin-dashboard";
     }
 
--- src/main/java/ju/example/training_management_system/service/AdminService/AdminService.java
+++ src/main/java/ju/example/training_management_system/service/AdminService/AdminService.java
@@ -1,4 +1,48 @@
 package ju.example.training_management_system.service.AdminService;
 
+import ju.example.training_management_system.model.users.Role;
+import ju.example.training_management_system.model.users.User;
+import ju.example.training_management_system.repository.AdvertisementRepository;
+import ju.example.training_management_system.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class AdminService {
+
+    private final UserRepository userRepository;
+    private final AdvertisementRepository advertisementRepository;
+
+    public void setUpAdminDashboard(Model model) {
+
+        List<User> users = userRepository.findAll();
+
+        long numOfCompanies = users.stream().
+                filter(user -> user.getRole() == Role.COMPANY).
+                count();
+
+        long numOfStudents = users.stream().
+                filter(user -> user.getRole() == Role.STUDENT).
+                count();
+
+        long numOfAdvertisements = advertisementRepository.findAll().size();
+
+        LocalDateTime now = LocalDateTime.now();
+        long newUsersCount = users.stream() // users who joined in the last 24 hours
+                .filter(user -> user.getJoinDate() != null &&
+                        ChronoUnit.HOURS.between(user.getJoinDate(), now) < 24)
+                .count();
+
+        model.addAttribute("usersCount", users.size());
+        model.addAttribute("companiesCount", numOfCompanies);
+        model.addAttribute("studentsCount", numOfStudents);
+        model.addAttribute("advertisementsCount", numOfAdvertisements);
+        model.addAttribute("newUsersCount", newUsersCount);
+    }
 }
